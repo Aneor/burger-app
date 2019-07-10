f4f41e223b0cba51d2af2028be2a608f48a39992
Aneor <aneor@ciandt.com>
2019-07-10 15:31:50 -0300

    Updating price

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -12,15 +12,21 @@ const controls = [
 const BuildControls = (props) =>
     (
         <div className={classes.BuildControls}>
+        <p>Current Price: <strong>{props.price.toFixed(2)}</strong></p>
             {
                 controls.map(ctrl => (
                     <BuildControl
                         key={ctrl.label}
                         label={ctrl.label}
                         added={() => props.ingredientsAdded(ctrl.type)}
-                        removed={() => props.ingredientsRemoved(ctrl.type)}></BuildControl>
+                        removed={() => props.ingredientsRemoved(ctrl.type)}
+                        disabled={props.disabled[ctrl.type]}
+                        ></BuildControl>
                 ))
             }
+            <button 
+                className={classes.OrderButton}
+                disabled={!props.purchaseable}>ORDER NOW</button>
         </div>
     );
 
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -5,9 +5,9 @@ import BuildControls from '../../components/Burger/BuildControls/BuildControls'
 
 const INGREDIENT_PRICES = {
     salad: 0.5,
-    cheese: 0.4,
-    meat: 1.3,
-    meat: 0
+    bacon: 0.2,
+    cheese: 0.6,
+    meat: 1.3
 }
 
 class BurgerBuilder extends Component {
@@ -26,12 +26,23 @@ class BurgerBuilder extends Component {
             cheese: 0,
             meat: 0
         },
-        totalPrice: 4
+        totalPrice: 4,
+        purchaseable: false
+    }
+
+    updatePurchaseState(ingredients) {
+        const sum = Object.keys(ingredients).map(key => {
+            return ingredients[key]
+        }).reduce((sum, el) => {
+            return sum + el;
+        }, 0);
+
+        this.setState({purchaseable: sum > 0});
     }
 
     addIngredientHandler = (type) => {
         const oldCount = this.state.ingredients[type];
-        const updatedCount = oldCount +1;
+        const updatedCount = oldCount + 1;
         const updatedIngredients = {
             ...this.state.ingredients
         }
@@ -42,12 +53,18 @@ class BurgerBuilder extends Component {
         const oldPrice = this.state.totalPrice;
         const newPrice = oldPrice + priceAddition;
 
-        this.setState({totalPrice: newPrice, ingredients: updatedIngredients});
+        this.setState({ totalPrice: newPrice, ingredients: updatedIngredients });
+        this.updatePurchaseState(updatedIngredients);
     }
 
     removeIngredientHangler = (type) => {
         const oldCount = this.state.ingredients[type];
-        const updatedCount = oldCount -1;
+
+        if (oldCount <= 0) {
+            return;
+        }
+
+        const updatedCount = oldCount - 1;
         const updatedIngredients = {
             ...this.state.ingredients
         }
@@ -58,16 +75,29 @@ class BurgerBuilder extends Component {
         const oldPrice = this.state.totalPrice;
         const newPrice = oldPrice - priceDeduction;
 
-        this.setState({totalPrice: newPrice, ingredients: updatedIngredients});
+        this.setState({ totalPrice: newPrice, ingredients: updatedIngredients });
+        this.updatePurchaseState(updatedIngredients);
     }
 
     render() {
+
+        const disableInfo = {
+            ...this.state.ingredients
+        };
+
+        for (let key in disableInfo) {
+            disableInfo[key] = disableInfo[key] <= 0;
+        }
+
         return (
             <Aux>
                 <Burger ingredients={this.state.ingredients} />
                 <BuildControls
                     ingredientsAdded={this.addIngredientHandler}
                     ingredientsRemoved={this.removeIngredientHangler}
+                    disabled={disableInfo}
+                    price={this.state.totalPrice}
+                    purchaseable={this.state.purchaseable}
                 />
             </Aux>
         );
